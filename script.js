6c0e2a0ecf571a57fcc04f2662bab4fedea3a239
keltech18 <94066678+keltech18@users.noreply.github.com>
2023-01-15 05:42:35 +0100

    changes implemented after review

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 
+const tableEl = document.querySelector("table");
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function myFunction() {
@@ -45,7 +48,7 @@ function myFunction2() {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.action')) {
+  if (!event.target.matches(".action")) {
     var dropdowns = document.getElementsByClassName("dropdown-content2");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -102,6 +105,7 @@ window.onclick = function(event) {
   }
 }
 
+
 function sidebar(){
   document.getElementById('bottomLeft').style.left="0";
   document.getElementById('navButton').style.display="none";
@@ -116,86 +120,129 @@ function sidebarClose(){
 }
 
 function addrow(){
-  const table3 = document.querySelector(".table3");
-  table3.innerHTML += `
-  <tr>
-      <td><input type="text" style="width: 20px;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-  </tr>
-  `;
-  e.preventDefault();
+  var table = document.getElementById("table3");
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  cell1.innerHTML = `<td><input type="text" style="width: 20px; font-size: 15px;"></td>`
+  cell2.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell3.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell4.innerHTML = `<td><button class="delete-row" id="deleterow" type="button"><i class="fa-solid fa-trash"></i></button></td>`
 }
 
 
 function addrow1(){
-  const table5 = document.querySelector(".table5");
-  table5.innerHTML += `
-  <tr>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-  </tr>
-  `;
-  e.preventDefault();
+  var table = document.getElementById("table5");
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
+  var cell7 = row.insertCell(6);
+  cell1.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell2.innerHTML = `<td><textarea style="width: 100%; height:50px; font-size: 15px;"></textarea></td>`
+  cell3.innerHTML = `<td><textarea style="width: 100%; height:50px; font-size: 15px;"></textarea></td>`
+  cell4.innerHTML = `<td><textarea style="width: 100%; height:50px; font-size: 15px;"></textarea></td>`
+  cell5.innerHTML = `<td><textarea style="width: 100%; height:50px; font-size: 15px;"></textarea></td>`
+  cell6.innerHTML = `<td><textarea style="width: 100%; height:50px; font-size: 15px;"></textarea></td>`
+  cell7.innerHTML = `<td><button class="delete-row" id="deleterow" type="button"><i class="fa-solid fa-trash"></i></button></td>`
 }
 
 function addrow2(){
-  const table6 = document.querySelector(".table6");
-  table6.innerHTML += `
-  <tr>
-      <td><input type="text" style="width: 20px;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-  </tr>
-  `;
-  e.preventDefault();
+  var table = document.getElementById("table6");
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5)
+  cell1.innerHTML = `<td><input type="text" style="width: 20px; font-size: 15px;"></td>`
+  cell2.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell3.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell4.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell5.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell6.innerHTML = `<td><button class="delete-row" id="deleterow" type="button"><i class="fa-solid fa-trash"></i></button></td>`
 }
 
 function addrow3(){
-  const table7 = document.querySelector(".table7");
-  table7.innerHTML += `
-  <tr>
-    <td><input type="text" style="width: 20px;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><input type="text" style="width: 100%;"></td>
-  </tr>
-  `;
-  e.preventDefault();
+  var table = document.getElementById("table7");
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+  var cell5 = row.insertCell(4);
+  var cell6 = row.insertCell(5);
+  var cell7 = row.insertCell(6);
+  cell1.innerHTML = `<td><input type="text" style="width: 20px; font-size: 15px;"></td>`
+  cell2.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell3.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell4.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell5.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell6.innerHTML = `<td><input type="text" style="width: 100%; font-size: 15px;"></td>`
+  cell7.innerHTML = `<td><button class="delete-row" id="deleterow" type="button"><i class="fa-solid fa-trash"></i></button></td>`
 }
+
+// function addrow4(){
+//   const table5 = document.querySelector(".table5");
+//   table5.innerHTML += `
+//   <tr>
+//       <td><input type="text" style="width: 100%;"></td>
+//       <td><textarea style="width: 100%; height:50px;"></textarea></td>
+//       <td><textarea style="width: 100%; height:50px;"></textarea></td>
+//       <td><textarea style="width: 100%; height:50px;"></textarea></td>
+//       <td><textarea style="width: 100%; height:50px;"></textarea></td>
+//       <td><textarea style="width: 100%; height:50px;"></textarea></td>
+//   </tr>
+//   `;
+// }
 
 function addrow4(){
-  const table5 = document.querySelector(".table5");
-  table5.innerHTML += `
-  <tr>
-      <td><input type="text" style="width: 100%;"></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-      <td><textarea style="width: 100%; height:50px;"></textarea></td>
-  </tr>
-  `;
-  e.preventDefault();
+  var table = document.getElementById("contacts");
+  var row = table.insertRow();
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = `<td><label><input type="checkbox"></label></td>`
+  cell2.innerHTML = `<td contenteditable="true">Enter name</td>`
+  cell3.innerHTML = `<td contenteditable="true">Enter email</td>`
 }
 
-function addrow4(){
-  const contacts = document.querySelector(".contacts");
-  contacts.innerHTML += `
-  <tr>
-    <td><label><input type="checkbox"></label></td>
-    <td>brain matt</td>
-    <td>bm1234@gmail.com</td>
-</tr>
-  `;
-  e.preventDefault();
-}
 
+// function deleteRow(e){
+//   if(!e.target.classList.contains("delete-row")){
+//     return;
+//   }
+
+//   var btnd = e.target;
+//   btnd.closest("tr").innerHTML.remove();
+// }
+
+// tableEl.addEventListener("click", deleteRow);
+
+$(document).ready(function() {
+
+  $(document).on('click',".delete-row", function (e) {
+   $(this).closest('tr').remove();
+  });
+});
+
+
+// var index, tableD = document.getElementById('table3');
+// for(var i = 1; i < tableD.rows.length; i++)
+// {
+//   tableD.rows[i].buttton.onclick = function()
+//   {
+//     var c = confirm("do you want to delete this row");
+//     if(c === true)
+//     {
+//       index = this.parentElement.rowindex;
+//       tableD.deleteRow(index);
+//     }
+//   };
+// }
